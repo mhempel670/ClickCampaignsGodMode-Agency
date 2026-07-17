45fac296a349a6bc0b1bc00f69126e1b4dbf2e84
mhempel670 <Matt@DigitalAccessPro.com>
2026-07-17 15:06:10 -0400

    Configure Graybeard funnel for root Vercel deployment

---
+++ api/create-addon-session.js
@@ -0,0 +1,2 @@
+module.exports = require('../clients/graybeard-guidance/campaigns/graybeard-assessment-funnel/output-assets/api/create-addon-session.js');
+
---
+++ api/create-checkout-session.js
@@ -0,0 +1,2 @@
+module.exports = require('../clients/graybeard-guidance/campaigns/graybeard-assessment-funnel/output-assets/api/create-checkout-session.js');
+
